1ff8e7cbb38aba84f4574e047b78cc6f30dd4df9
Arhipkin Ilya <ilya@arhipkin.com>
2018-05-27 23:57:00 +0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: sex
+ *
+ * Created on 18 Февраль 2014 г., 20:02
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
